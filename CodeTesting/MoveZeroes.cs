bbd9be867aa5fb7c17a8c9620dcc5543197de0d0
David Clayton <david@fintools.com>
2020-08-09 18:33:03 -0700

    Move Zeros Problem

---
+++ CodeTesting/MoveZeroes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code_Challenges
+{
+    public class MoveZeroes
+    {
+        /*Given an array nums, write a function to move all 0's 
+         to the of it while maintaining the relative order of the non 
+         zero elements
+         1.  You must do this in-place without making a copy of the array
+         2.  Minimize the total number of operations*/
+
+         public static void MoveZeroes_Solution(ref int[] nums)
+        {
+            int count = nums.Length;
+
+            for(int i=0;i<count;i++)
+            {
+                if(nums[i]==0 && i!= count-1)
+                {
+                    int last = nums[count - 1];
+                    nums[count - 1] = nums[i];
+
+                    for(int j=i;j<count-2;j++)
+                    {
+                        nums[j] = nums[j + 1];
+                    }
+                    nums[count - 2] = last;
+                }
+            }
+
+            return;
+        }
+    }
+}
---
+++ Code_Tests/MoveZeroes_Test.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Code_Challenges;
+
+namespace Code_Tests
+{
+    [TestClass]
+    public class MoveZeroes_Test
+    {
+        [TestMethod]
+        public void ExampleProblem()
+        {
+            //Arrange
+            int[] input = { 0, 1, 0, 3, 12 };
+            int[] expected = { 1, 3, 12, 0, 0 };
+
+            //Act
+            MoveZeroes.MoveZeroes_Solution(ref input);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, input);
+        }
+
+        [TestMethod]
+        public void TwoIndex()
+        {
+            //Arrange
+            int[] input = { 0, 1};
+            int[] expected = { 1,0};
+
+            //Act
+            MoveZeroes.MoveZeroes_Solution(ref input);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, input);
+        }
+
+        [TestMethod]
+        public void ThreeIndex()
+        {
+            //Arrange
+            int[] input = { 0, 0, 1 };
+            int[] expected = { 1,0, 0 };
+
+            //Act
+            MoveZeroes.MoveZeroes_Solution(ref input);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, input);
+        }
+    }
+}
