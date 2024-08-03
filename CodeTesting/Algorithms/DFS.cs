599947af7a6ceec462e6d6916ce75a0fa6b2d5f4
David Clayton <david@davidclayton.me>
2024-08-03 11:15:00 -0700

    DFS Brute Force

--- CodeTesting/Algorithms/DFS.cs
+++ CodeTesting/Algorithms/DFS.cs
@@ -15,6 +15,90 @@ namespace Code_Challenges.Algorithms
             int rows = grid.GetLength(0);
             if (rows == 0) return -1;
             int cols = grid.GetLength(1);
+
+            int numIslands = 0;
+
+            Stack<int[,]> nodeStack = new Stack<int[,]>();
+
+            //Enqueue 1 as a root, changing to 0 to note it's been visited
+            for (int gridRow = 0; gridRow < rows; gridRow++)
+            {
+                for (int gridCol = 0; gridCol < cols; gridCol++)
+                {
+                    if (grid[gridRow, gridCol] == '1')
+                    {
+                        int[,] node = new int[1, 2];
+                        node[0, 0] = gridRow;
+                        node[0, 1] = gridCol;
+
+                        nodeStack.Push(node);
+                        grid[gridRow, gridCol] = '0';
+
+                        numIslands++;
+
+                        while (nodeStack.Count > 0)
+                        {
+                            int[,] curNode = nodeStack.Pop();
+
+                            int curRow = curNode[0, 0];
+                            int curCol = curNode[0, 1];
+
+                            //Left
+                            if (curRow > 0 && grid[curRow - 1, curCol] == '1')
+                            {
+                                int[,] newNode = new int[1, 2];
+                                newNode[0, 0] = curRow - 1;
+                                newNode[0, 1] = curCol;
+                                //Add node to queue and mark as visited
+                                nodeStack.Push(newNode);
+                                grid[curRow - 1, curCol] = '0';
+                            }
+
+                            //Right
+                            if (curRow < rows - 1 && grid[curRow + 1, curCol] == '1')
+                            {
+                                int[,] newNode = new int[1, 2];
+                                newNode[0, 0] = curRow + 1;
+                                newNode[0, 1] = curCol;
+                                //Add node to queue and mark as visited
+                                nodeStack.Push(newNode);
+                                grid[curRow + 1, curCol] = '0';
+                            }
+
+                            //Up
+                            if (curCol > 1 && grid[curRow, curCol - 1] == '1')
+                            {
+                                int[,] newNode = new int[1, 2];
+                                newNode[0, 0] = curRow;
+                                newNode[0, 1] = curCol - 1;
+                                //Add node to queue and mark as visited
+                                nodeStack.Push(newNode);
+                                grid[curRow - 1, curCol - 1] = '0';
+                            }
+
+                            //Down
+                            if (curCol < cols - 1 && grid[curRow, curCol + 1] == '1')
+                            {
+                                int[,] newNode = new int[1, 2];
+                                newNode[0, 0] = curRow;
+                                newNode[0, 1] = curCol + 1;
+                                //Add node to queue and mark as visited
+                                nodeStack.Push(newNode);
+                                grid[curRow, curCol + 1] = '0';
+                            }
+                        }
+                    }
+                }
+            }
+
+            return numIslands;
+        }
+
+        public static int NumIslandsDFS_Recursive(char[,] grid)
+        {
+            int rows = grid.GetLength(0);
+            if (rows == 0) return -1;
+            int cols = grid.GetLength(1);
             int numIslands = 0;
 
             for(int row = 0; row<rows;row++)
