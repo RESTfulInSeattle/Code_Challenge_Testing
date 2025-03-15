0dd49738bf86573f79488be55270d27c6d43f7a6
David Clayton <david@davidclayton.me>
2025-03-15 13:16:01 -0700

    started djisktras

---
+++ CodeTesting/Algorithms/Dijkstras.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Challenges.Algorithms
+{
+    class Dijkstras
+    {
+        private Dictionary<string, HashSet<string>> _adjacencyList;
+
+        private void AddEdge(string v1, string v2)
+        {
+            if (!_adjacencyList.ContainsKey(v1))
+            {
+                _adjacencyList[v1] = new HashSet<string>();
+            }
+            _adjacencyList[v1].Add(v2);
+        }
+
+
+    }
+}
