67a86e424746bff62ac790b4509a38791b8663c8
David Clayton <david@davidclayton.me>
2025-10-02 11:47:48 -0700

    Added new classes

---
+++ CodeTesting/Algorithms/Topological Sort.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Challenges.Algorithms
+{
+    internal class Topological_Sort
+    {
+    }
+}
+
+/*
+ * Based on DFS
+ * Graph with directed edges and no cycles
+ * 
+ * Timne:  O(N)
+ * 
+ * Data Structure:  Hash Set
+ * 
+ * */
---
+++ CodeTesting/Algorithms/UnionFind.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Challenges.Algorithms
+{
+    internal class UnionFind
+    {
+    }
+}
+
+/*
+ * Union together disjointed sets
+ *Time:
+ *  O(NlogN)
+ * 
+ * Data structures:
+ *  Forest of trees
+ *  
+ *  
+ */ 
